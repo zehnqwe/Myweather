5f34eab57c53f2e6338a1d2ed7a4d0d54a958770
zehnqwe <1148178442@qq.com>
2017-02-28 17:52:17 +0800

    first commit

---
+++ app/src/main/java/com/example/cn/dp/City.java
@@ -0,0 +1,46 @@
+package com.example.cn.dp;
+
+
+
+/**
+ * Created by 张振 on 2017/2/27.
+ */
+
+public class City extends DataSupport{
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/example/cn/dp/Province.java
@@ -0,0 +1,37 @@
+package com.example.cn.dp;
+
+
+
+/**
+ * Created by 张振 on 2017/2/27.
+ */
+
+public class Province extends DataSupport{
+private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
